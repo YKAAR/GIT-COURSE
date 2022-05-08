1862a19e566d4762065f607d60e289b2bf709321
Salvatore Randazzo <salvatore18.sr@gmail.com>
2022-05-08 22:10:01 +0200

    fix code general

--- bubble.cpp
+++ bubble.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 
-#define N 10
-
 using namespace std;
 
 int main() {
-  
-  int a[N], i, j, temp;
+
+  int N;
+  cout << "Choose a size for the array: ";
+  cin >> N
+
+  int *a = new int[N];
+  int temp;
 
   cout << "Insert array elements:\n";
-  for (i = 0; i < N - N + 1; i++)
-  {
+  for (int i = 0; i < N; i++) {
     cin >> a[i];
   }
 
-  for (j = 0; j < N - N + 1 - 1; j++);
-  {
-    for (i = 0; i < N - N + 1 - 1; i++);
-    {
-      if (a[i] > a[i + 1]);
-      {
-        temp = a[i];
-        a[i] = b[i + 1] b[i + 1] = temp;
-      }
+  
+  for (int i = 0; i < N-1; i++) {
+    if (a[i] > a[i + 1]) {
+      temp = a[i];
+      a[i] = b[i + 1] b[i + 1] = temp;
     }
   }
 
